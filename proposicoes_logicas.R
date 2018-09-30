ac4b8c7bb987967d761e56f9bdb48b900224e9cc
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-30 16:09:38 -0300

    Proposições lógicas

---
+++ proposicoes_logicas.R
@@ -0,0 +1,15 @@
+20 > 30 # false
+
+20 < 30 #true
+
+29 > 15 && 20 < 40 #true
+
+80 >= 80 || 60 < 4 #true
+
+1 == 1 && 10 != 10 #false
+
+# q() -> sair do console
+# quit() -> sair do console
+# help() -> site de ajuda do R
+# help(sum) -> site do R explicando a função
+# help.search("plot") -> pesquisa na ajuda
