f4ef7965632d8b16688354c5eb5116a7bbeb2172
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-30 16:21:05 -0300

    Objeto

---
+++ objetos.R
@@ -0,0 +1,17 @@
+# Criando objetos
+valor <- 10
+pares <- c(0, 2, 4, 6, 8)
+cores <- c("verde", "vermelho", "amarelo")
+
+
+# Mostrando
+ls()
+valor
+pares
+cores
+
+# Declarando objeto de formas diferentes
+50 -> num1
+num2 <- 20
+soma = num1 + num2
+soma
