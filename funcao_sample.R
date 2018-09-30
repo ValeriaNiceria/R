16375bf33cc4d13bf69088768dc6216254532920
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-30 16:32:31 -0300

    Função - Sample

---
+++ funcao_sample.R
@@ -0,0 +1,17 @@
+#sequência de 1 a 100
+1:100
+
+# Usando a função sample
+moeda <- c("cara", "coroa")
+moeda
+sample(moeda, 1)
+
+# Gerando números para loteria
+sorteio <- c(1:25)
+sorteio
+sample(sorteio, 15)
+
+# Girando o dado
+dado <- c(1:6)
+dado
+sample(dado, 1)
