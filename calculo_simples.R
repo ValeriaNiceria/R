86ca8819ba80a746ee47db30d8092f8c2e7089c9
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-30 15:44:54 -0300

    Calculo simples

---
+++ calculo_simples.R
@@ -0,0 +1,23 @@
+a <- 5
+b <- 10
+
+soma = a + b
+soma
+
+subtracao = b - a
+subtracao
+
+multiplicacao = a * b
+multiplicacao
+
+divisao = a / b
+divisao
+
+potencia1 = a ^ b
+potencia1
+
+potencia2 = a ** b
+potencia2
+
+calc = 30 + 20 * 2 ^ 3
+calc
