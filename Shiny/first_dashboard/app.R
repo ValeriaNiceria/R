9eabd116416fc48c3819a7223614293cc9b4cff4
ValeriaNiceria <valerianiceria@gmail.com>
2019-04-10 20:34:46 -0300

    start dashboard shiny

---
+++ Shiny/first_dashboard/app.R
@@ -0,0 +1,14 @@
+install.packages(c("shiny", "shinydashboard"))
+
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui, server)
