a3412cec7f510783d5178d189588ebf0644086b6
ValeriaNiceria <valerianiceria@gmail.com>
2019-04-16 23:08:11 -0300

    new input

--- Shiny/datacamp/first/script.R
+++ Shiny/datacamp/first/script.R
@@ -24,7 +24,12 @@ ui <- fluidPage(
       selectInput(inputId = "x",
                   label = "X-axis:",
                   choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
-                  selected = "imdb_num_votes")
+                  selected = "imdb_num_votes"),
+      # Select variable for color
+      selectInput(inputId = "z",
+                  label = "Color by:",
+                  choices = c("title_type", "genre", "mpaa_rating", "critics_rating", "audience_rating"),
+                  selected = "mpaa_rating")
     ),
     
     # Outputs
@@ -40,7 +45,7 @@ server <- function(input, output) {
   
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
-    ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
+    ggplot(data = movies, aes_string(x = input$x, y = input$y), color = input$z) +
       geom_point()
   })
 }
