59ea0034f9c99c0b12d032bf736c2c117ccdf5a3
ValeriaNiceria <valerianiceria@gmail.com>
2019-04-27 13:31:39 -0300

    Conceitos básicos

---
+++ Ciência de dados/CONCEITOS BÁSICOS/Funções/main.R
@@ -0,0 +1,20 @@
+# Funções R
+
+# Soma
+result <- sum(5, 19)
+
+# Média
+result <- mean(2, 3, 4, 5, 6)
+
+# Variância
+result <- var(1:5, 11:15)
+
+# Mediana
+result <- median(1:6)
+
+# Resumo Estatístico
+summary(iris)
+
+
+# Quantis
+quantile(rnorm(10))
---
+++ Ciência de dados/CONCEITOS BÁSICOS/Pacotes/main.R
@@ -0,0 +1,9 @@
+# Pacotes
+install.packages("dplyr") # instala um famoso pacote de manipulação de dados
+
+# Carregando o pacote
+library(dplyr)
+
+install.packages("tidyverse")
+
+?dplyr
