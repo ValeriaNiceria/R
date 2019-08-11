e3120696ece924fea1956dc5e53cd4ec46ed10dc
ValeriaNiceria <valerianiceria@gmail.com>
2019-08-11 20:30:29 -0300

    update

--- Visualização/Introdução ao ggplot2/app.R
+++ Visualização/Introdução ao ggplot2/app.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 # Pacote janitor para limpar os nomes das colunas
 library(janitor)
-library(readxl)
 # Metadados gerais de países
 library(countrycode)
 # Combinar vários gráficos do ggplot2 em um só
@@ -10,12 +9,8 @@ library(patchwork) # devtools::install_github("thomasp85/patchwork")
 sessionInfo()
 
 
-# baixe o arquivo do endereco:
-# download.file("https://s3.amazonaws.com/happiness-report/2018/WHR2018Chapter2OnlineData.xls", destfile = "felicidade.xls")
-
-
 # importar a planilha
-df_pais <-  read.xlsx("felicidade.xls")
+df_pais <-  read.csv("felicidade.csv")
 # limpar o nome das colunas
 df_pais <- janitor::clean_names(df_pais)
 # olhar dados 
