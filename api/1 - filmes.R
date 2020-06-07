e1bb5b836cee7a309e69cfb89f97cdf498b359e9
ValeriaNiceria <valerianiceria@gmail.com>
2020-06-07 14:28:11 -0300

    api plumber

--- api/1 - filmes.R
+++ api/1 - filmes.R
@@ -1,40 +1,6 @@
 # opencpu
 # plumber
 
-# Filmes IMDB ------------------
-if (!require(ggplot2movies)) install.packages("ggplot2movies")
-
-library(ggplot2movies)
-library(tidyverse)
-
-dados <- ggplot2movies::movies 
-View(dados)
-
-dados <- dados %>%
-  filter(!is.na(budget), budget > 0) %>% 
-  dplyr::select(title, year, budget, rating) %>% 
-  arrange(desc(year))
-
-
-# Tentando prever a nota utilizando as variáveis 'budget' e 'year'
-modelo <- lm(rating ~ budget + year, data = dados)
-summary(modelo)
-
-prever_nota_filme <- function(orcamento, ano) {
-  predict(
-    modelo, 
-    newdata = data.frame(
-      budget = orcamento, 
-      year = ano
-    )
-  )
-}
-
-amostrar_dez_filmes <- function(dados) {
-  dados %>%
-    sample_n(10)
-}
-
 
 # Criar API plumber, necessário:
 # - Uma função que executa uma ação
@@ -45,5 +11,9 @@ library(plumber)
 
 # Rodar exemplo api usando plumber
 p <- plumber::plumb("R/funcoes_api.R")
-p$run(port = 3333)
+p$run(port = 8080, host="127.0.0.1")
+
+# curl -X GET "http://127.0.0.1:8080/amostrar_filmes"
+# curl --data "orcamento=10000&ano=1991" "http://localhost:8080/prever_nota_filme"
+
 
--- api/R/funcoes_api.R
+++ api/R/funcoes_api.R
@@ -1,6 +1,34 @@
-# * @get / solta
+# Filmes IMDB ------------------
+if (!require(ggplot2movies)) install.packages("ggplot2movies")
+
+library(ggplot2movies)
+library(tidyverse)
+
+dados <- ggplot2movies::movies 
+dados <- dados %>%
+  filter(!is.na(budget), budget > 0) %>% 
+  dplyr::select(title, year, budget, rating) %>% 
+  arrange(desc(year))
+
+# Tentando prever a nota utilizando as variáveis 'budget' e 'year'
+modelo <- lm(rating ~ budget + year, data = dados)
+# summary(modelo)
+
+# -----------------------------------------------------
+
+#* @get /amostrar_filmes
 amostrar_filmes <- function() {
   dados %>% 
     sample_n(10)
 }
 
+
+#* @post /prever_nota_filme
+prever_nota_filme <- function(orcamento, ano) {
+  d <- data.frame(
+    budget = as.numeric(orcamento),
+    year = as.numeric(ano)
+  ) 
+  
+  predict(modelo, newdata = d)
+}
